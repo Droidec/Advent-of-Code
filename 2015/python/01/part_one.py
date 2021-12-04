1aabb8c160cc2ab2adc272dc228a33887f542aff
Droidec <derovor@gmail.com>
2021-12-04 23:37:46 +0100

    Handle IOError in Python

--- 2015/python/01/part_one.py
+++ 2015/python/01/part_one.py
@@ -10,13 +10,17 @@ if __name__ == '__main__':
     floor = 0
     direction = {'(': 1, ')': -1}
 
-    with open(INPUT_FILE, 'r', encoding='UTF-8') as file:
-        inst = file.read()
-        for index, char in enumerate(inst):
-            if char in direction:
-                floor += direction[char]
-            else:
-                sys.stderr.write(f"Invalid character found at position '{index+1}'\n")
-                sys.exit(1)
+    try:
+        with open(INPUT_FILE, 'r', encoding='UTF-8') as file:
+            inst = file.read()
+            for index, char in enumerate(inst):
+                if char in direction:
+                    floor += direction[char]
+                else:
+                    sys.stderr.write(f"Invalid character found at position '{index+1}'\n")
+                    sys.exit(1)
+    except IOError as e:
+        sys.stderr.write(f"{e}\n")
+        sys.exit(1)
 
     print(f"The right floor is '{floor}'")
--- 2015/python/01/part_two.py
+++ 2015/python/01/part_two.py
@@ -4,24 +4,28 @@ Advent of Code 2015 day 01 part one
 
 import sys
 
-INPUT_FILE = "../../inputs/01-input.txt"
+INPUT_FILE = "../inputs/01-input.txt"
 
 if __name__ == '__main__':
     floor = 0
     direction = {'(': 1, ')': -1}
 
-    with open(INPUT_FILE, 'r', encoding='UTF-8') as file:
-        inst = file.read()
-        for index, char in enumerate(inst):
-            if char in direction:
-                floor += direction[char]
-            else:
-                sys.stderr.write(f"Invalid character found at position '{index+1}'\n")
-                sys.exit(1)
+    try:
+        with open(INPUT_FILE, 'r', encoding='UTF-8') as file:
+            inst = file.read()
+            for index, char in enumerate(inst):
+                if char in direction:
+                    floor += direction[char]
+                else:
+                    sys.stderr.write(f"Invalid character found at position '{index+1}'\n")
+                    sys.exit(1)
 
-            if floor == -1:
-                print(f"Basement is reached at position '{index+1}'")
-                sys.exit(0)
+                if floor == -1:
+                    print(f"Basement is reached at position '{index+1}'")
+                    sys.exit(0)
+    except IOError as e:
+        sys.stderr.write(f"{e}\n")
+        sys.exit(1)
 
     sys.stderr.write("The basement was never reached?\n")
     sys.exit(1)
