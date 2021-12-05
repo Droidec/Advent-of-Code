046efad7f550c04754ab3f6494e4515181fe4ed5
Droidec <derovor@gmail.com>
2021-12-05 12:16:42 +0100

    Resolve AoC 2015 day 01 part one in Rust

--- 2015/rust/01/part_one.rs
+++ 2015/rust/01/part_one.rs
@@ -2,14 +2,26 @@
  * Advent of Code 2015 day 01 part one
  */
 
-use std::fs::File;
-use std::io::BufReader;
+use std::fs::File; // File::open
+use std::io::Read; // read_to_string
 
 // String slice constant
 const INPUT_FILE: &str = "../inputs/01-input.txt";
 
 fn main() {
-    let mut file = BufReader::new(
-        File::open(INPUT_FILE).expect(&format!("Failed to open file '{}'", INPUT_FILE)),
-    );
+    let mut file = File::open(INPUT_FILE).expect(&format!("Failed to open file '{}'", INPUT_FILE));
+    let mut buffer = String::new();
+    let mut floor = 0;
+
+    file.read_to_string(&mut buffer).expect("read_to_string failed");
+
+    for (index, ch) in buffer.chars().enumerate() {
+        match ch {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => panic!("Invalid character found at position {}", index + 1),
+        };
+    }
+
+    println!("The right floor is {}", floor);
 }
