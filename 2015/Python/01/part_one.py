c293d03bf8eacf03223c9daf38b02d8b5f08aa26
Droidec <derovor@gmail.com>
2021-12-05 13:32:31 +0100

    Restore correct directories

---
+++ 2015/Python/01/part_one.py
@@ -0,0 +1,30 @@
+"""
+Advent of Code 2015 day 01 part one
+"""
+
+import sys
+
+INPUT_FILE = "../inputs/01-input.txt"
+
+if __name__ == '__main__':
+    floor = 0
+    index = 0
+    direction = {'(': 1, ')': -1}
+
+    try:
+        with open(INPUT_FILE, 'r', encoding='UTF-8') as file:
+            while True:
+                inst = file.read(1)
+                if not inst:
+                    break # EOF
+                if inst[0] in direction:
+                    floor += direction[inst[0]]
+                else:
+                    sys.stderr.write(f"Invalid character found at position '{index+1}'\n")
+                    sys.exit(1)
+                index += 1
+    except IOError as e:
+        sys.stderr.write(f"{e}\n")
+        sys.exit(1)
+
+    print(f"The right floor is '{floor}'")
