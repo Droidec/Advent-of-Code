058f2a6842c9976053d8c9f3293502bd2e4ffd65
Droidec <derovor@gmail.com>
2021-12-05 13:27:18 +0100

    Upper case directories

---
+++ 2015/Python/README.md
@@ -0,0 +1,17 @@
+# Advent of Code 2015 in Python
+
+This directory contains all the puzzles solved in [Python](https://www.python.org/) for the Advent of Code 2015 event.
+
+## Version
+
+Developed on a Mac with `python`/`pylint` packages contained in [brew](https://brew.sh/).  
+Reference version: `Python 3.9.9`.
+
+## Commands
+
+- `make run` to run all sources at once
+
+## Miscellaneous
+
+Sources are assumed to be executed at the root of this README (e.g. `python3 01/part_one.py`).  
+All sources have been passed to `pylint` for quality check.
---
+++ 2015/Rust/01/part_one.rs
@@ -0,0 +1,42 @@
+/*
+ * Advent of Code 2015 day 01 part one
+ */
+
+use std::fs::File; // File::open
+use std::io::BufReader; // BufReader::new
+use std::io::Read; // read
+use std::process; // process::exit
+
+// String slice constant
+const INPUT_FILE: &str = "../inputs/01-input.txt";
+
+fn main() {
+    let file = File::open(INPUT_FILE).expect(&format!("Failed to open file '{}'", INPUT_FILE));
+    let mut reader = BufReader::new(file); // optimize reading
+    let mut buffer = [0; 1];
+    let mut floor = 0;
+    let mut index = 0;
+
+    loop {
+        let num_bytes = reader.read(&mut buffer).expect("read failed"); // Read 1 byte
+        if num_bytes == 0 {
+            break; // EOF
+        }
+
+        let inst = match std::str::from_utf8(&buffer) {
+            Ok(ch) => ch,
+            Err(_) => panic!("Invalid UTF-8 character found at position '{}'", index + 1),
+        };
+
+        match inst.chars().next().unwrap() {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => panic!("Invalid character found at position '{}'", index + 1),
+        };
+
+        index += num_bytes;
+    }
+
+    println!("The right floor is '{}'", floor);
+    process::exit(0);
+}
