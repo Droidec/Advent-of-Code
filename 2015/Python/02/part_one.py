d938a9555f7318070121b88a378b292d26bd0969
Droidec <derovor@gmail.com>
2021-12-05 23:26:26 +0100

    Update AoC 2015 day 02 in Python

--- 2015/Python/02/part_one.py
+++ 2015/Python/02/part_one.py
@@ -6,27 +6,19 @@ import sys
 
 INPUT_FILE = "../inputs/02-input.txt"
 
-def calculate_surface_area(l=0, w=0, h=0):
-    return 2*l*w + 2*w*h + 2*h*l
-
 if __name__ == '__main__':
     total = 0
 
     try:
         with open(INPUT_FILE, 'r', encoding='UTF-8') as file:
-            while True:
-                box_dim = file.readline();
-                if not box_dim:
-                    break # EOF
-
-                list_dim = box_dim.split('x')
-                l = int(list_dim[0])
-                w = int(list_dim[1])
-                h = int(list_dim[2])
-
-                total += calculate_surface_area(l, w, h);
-                total += min(min(l*h, h*w), l*w)
-
+            for line in file:
+                l, w, h = line.split('x')
+                l, w, h = int(l), int(w), int(h)
+                s1 = l * w
+                s2 = l * h
+                s3 = w * h
+                extra = min(s1, s2, s3)
+                total += 2*s1 + 2*s2 + 2*s3 + extra
     except IOError as e:
         sys.stderr.write(f"{e}\n")
         sys.exit(1)
--- 2015/Python/02/part_two.py
+++ 2015/Python/02/part_two.py
@@ -11,17 +11,11 @@ if __name__ == '__main__':
 
     try:
         with open(INPUT_FILE, 'r', encoding='UTF-8') as file:
-            while True:
-                box_dim = file.readline();
-                if not box_dim:
-                    break # EOF
-
-                list_dim = box_dim.split('x')
-                list_dim = [int(dim) for dim in list_dim]
-                list_dim = sorted(list_dim)
-                total += list_dim[0]*2 + list_dim[1]*2
-                total += list_dim[0]*list_dim[1]*list_dim[2]
-
+            for line in file:
+                dims = line.split('x')
+                dims = sorted([int(dim) for dim in dims])
+                total += dims[0]*2 + dims[1]*2
+                total += dims[0]*dims[1]*dims[2]
     except IOError as e:
         sys.stderr.write(f"{e}\n")
         sys.exit(1)
