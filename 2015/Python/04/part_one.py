6e092616a6586115bd111e2e39ab9bdabac9b351
Droidec <derovor@gmail.com>
2021-12-17 23:59:33 +0100

    Resolve AoC 2015 day 04 in Python

---
+++ 2015/Python/04/part_one.py
@@ -0,0 +1,19 @@
+"""
+Advent of Code 2015 day 04 part one
+"""
+
+import hashlib
+
+if __name__ == '__main__':
+    key = 'bgvyzdsv'
+    value = 1
+    while True:
+        seed = key + str(value)
+        md5 = hashlib.md5(seed.encode())
+        md5_hash = md5.hexdigest()
+
+        if md5_hash.startswith('00000'):
+            break
+        value += 1
+
+    print(f"The answer is '{value}'")
